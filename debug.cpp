9e74dbbf57a8344cd537e56aab499b02f9ea93c4
Li Zhikun <1668676243@qq.com>
2023-08-08 18:22:12 +0800

    上传debug.cpp文件
    第一次上传文件

---
+++ debug.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#define N 32
+using namespace std;
+int main()
+{
+    int a = 10;
+    const int A = 30; 
+    cout << "a = " << a << endl; 
+    cout << "一共有A = " << A << "个人" << endl;
+    cout << "一共有N = " << N << "个世界"<< endl;
+    system("pause");
+    return 0;
+}
